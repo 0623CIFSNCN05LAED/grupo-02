5459e7fa570249a3d48a9ff378a3cca4f4d7066c
Alfredo-Al <alfredo-ar2696@hotmail.com>
2023-09-25 21:05:42 -0500

    Add create and delete service and visual changes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,13 +1,37 @@
 const express = require ('express');
 const router = express.Router();
+const path = require("path");
+const multer = require("multer");
+
+
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, "../../public/images/products"),
+    filename: function (req, file, cb) {
+      cb(
+        null,
+        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  });
+  
+  const upload = multer({
+    storage: storage,
+  });
+
 
 const productController = require ('../controllers/productController.js');
 
-
 router.get('/cart', productController.cart);
-router.get('/create',productController.create);
-router.get("/:id/", productController.detail);
-router.get('/edit',productController.edit);
 
+router.get('/create',productController.create);
+router.post("/home", upload.single("archivo"), productController.store);
+
+router.get("/detail/:id/", productController.detail);
+
+
+router.get("/edit/:id", productController.edit);
+router.put("/update/:id", upload.single("archivo"), productController.update);
+
+router.delete("/:id", productController.destroy);
 
 module.exports = router;
--- src/services/productsServices.js
+++ src/services/productsServices.js
@@ -2,12 +2,21 @@ const db = require("../data/db");
 
 const productServices = {
     getAllProducts: () => {
-      return db.products.find();
+      return db.products.findAll();
     },
     getProduct: (id) => {
       const product = db.products.findById(id);
       return product;
     },
+    createProduct: (product) => {
+      db.products.create(product);
+    },
+    updateProduct: (id, product) => {
+      db.products.update(id, product);
+    },
+    deleteProducts: (id) => {
+      db.products.delete(id);
+    }
 }
 
 module.exports = productServices;
